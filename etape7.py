42ff129e9cfa97d74cb0dcbab568b93985b9ecc7
Ali Guenbour <aliguenbou07@gmail.com>
2026-03-12 02:48:52 +0800

    migration PostgreSQL

--- etape7.py
+++ etape7.py
@@ -8,7 +8,11 @@ import pandas as pd, warnings
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pilar.db"
+import os
+db_url = os.environ.get("DATABASE_URL", "sqlite:///pilar.db")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
